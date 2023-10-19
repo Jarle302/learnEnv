cec57320cd6f6a41f0fdb3cd747430f8ae02dec2
Jarle302 <jarlehtollaksen2@live.no>
2023-10-19 12:28:30 +0200

    add test

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
 require ("dotenv/config")
+
+
+function  test(expectedSecret){
+    const secretMatches = process.env.MEANING_OF_LIFE == expectedSecret;
+    console.log(`the secret ${secretMatches?"":"not"}matches`)
+    console.log(`the meaning of life is ${process.env.MEANING_OF_LIFE}`)
+    console.log(secretMatches)
+}
+
+test(42)
